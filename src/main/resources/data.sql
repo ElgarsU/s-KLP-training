4778017ddb023af6b2e3886a8c1d4bbd29024a87
Elgars Upitis <elgars.upitis@accenture.com>
2022-06-15 11:57:23 +0300

    Migrate database to H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO fish (id, name, restricted) VALUES (1, 'Salmon', 1);
+INSERT INTO fish (id, name, restricted) VALUES (2, 'Pike', 0);
+INSERT INTO fish (id, name, restricted) VALUES (3, 'Perch', 0);
