819fdfd251c01162e405ace99ba61d609f109b04
Elgars Upitis <elgars.upitis@accenture.com>
2022-06-15 17:40:31 +0300

    Add flyway scripts for lakeDB

---
+++ src/main/resources/db/migration/V1_3__lakeDB_fill.sql
@@ -0,0 +1,3 @@
+INSERT INTO lake (id, name, depth) VALUES (1, 'Burtnieks', 4);
+INSERT INTO lake (id, name, depth) VALUES (2, 'Ungurs', 7);
+INSERT INTO lake (id, name, depth) VALUES (3, 'Lubāns', 3);
