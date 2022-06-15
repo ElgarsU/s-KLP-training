dca58b5a4d3b4c642ba745d675c3b9b1be3feeee
Elgars Upitis <elgars.upitis@accenture.com>
2022-06-15 15:18:23 +0300

    Configure MySQL local database

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO fish (id, name, restricted) VALUES (1, 'Salmon', 1);
-INSERT INTO fish (id, name, restricted) VALUES (2, 'Pike', 0);
-INSERT INTO fish (id, name, restricted) VALUES (3, 'Perch', 0);
